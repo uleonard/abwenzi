519cc9f3fa04432b55402b0e56fdc79e632ed2c9
Umali Leonard <uleonard@poly.ac.mw>
2020-06-27 12:15:19 +0200

    Added 1:M between LoanType and Loan models

--- app/Loan.php
+++ app/Loan.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
-    //
+    
+    /**
+     * Get the loan type that owns the loan.
+     */
+    public function loan_types()
+    {
+        return $this->belongsTo('App\LoanType', 'loan_type');
+
+    }
+
 }
+
--- app/LoanType.php
+++ app/LoanType.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoanType extends Model
 {
-    //
+    
+     /**
+     * Get the loans for the loan category.
+     */
+    public function loans()
+    {
+        return $this->hasMany('App\Loan', 'loan_type');
+
+    }
 }
